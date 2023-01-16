1cd3aa8ee4da45bb74007a871117fe7d9ab44cab
hamidreza <haimdreza121212@gmail.com>
2023-01-17 00:43:26 +0330

    add css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,12 @@
 <template>
+  <div>
+    <Header />
     <div>
-        <header class="flex-auto container">
-            <nav>
-                <ul>
-                    <li>
-                        <NuxtLink to="/">Home</NuxtLink> 
-                    </li>
-                    <li>
-                        <NuxtLink to="/about">About</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/products">Products</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-
-        <div class="container p-2">
-            <slot />
-        </div>
-
+      <slot />
     </div>
+
+    <Footer />
+  </div>
 </template>
 
 <script setup>
@@ -28,10 +14,5 @@
 </script>
 
 <style scoped>
-
-.router-link-active {
-    color: rgb(61, 180, 180);
-}
-
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,494 @@
 <template>
-    <div>
+  <div>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
+    <h2>Home</h2>
 
-        <h2>Home</h2>
+    <section class="course-wrapper section-padding bg-gray">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-8">
+            <div class="section-heading mb-70 text-center">
+              <h2 class="font-lg">Popular Courses</h2>
+              <p>Discover Your Perfect Program In Our Courses.</p>
+            </div>
+          </div>
+        </div>
 
-        <p> sit amet consectetur adipisicing elit. Velit libero quibusdam repudiandae sint nobis veritatis ullam non quam architecto odit omnis consequatur aut facilis at impedit, dignissimos, sunt consectetur est.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit libero quibusdam repudiandae sint nobis veritatis ullam non quam architecto odit omnis consequatur aut facilis at impedit, dignissimos, sunt consectetur est.</p>
+        <div class="row justify-content-lg-center">
+          <div class="col-xl-3 col-lg-6 col-md-6">
+            <div class="course-grid tooltip-style bg-white hover-shadow">
+              <div class="course-header">
+                <div class="course-thumb">
+                  <img
+                    src="~/assets/images/course/img_01.jpg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                  <div class="course-price">$300</div>
+                </div>
+              </div>
 
-    </div>
+              <div class="course-content">
+                <div class="rating mb-10">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+
+                  <span>3.9 (30 reviews)</span>
+                </div>
+                <h3 class="course-title mb-20">
+                  <a href="#">SQL-Data Analysis: Crash Course</a>
+                </h3>
+                <div
+                  class="course-footer mt-20 d-flex align-items-center justify-content-between"
+                >
+                  <span class="duration"
+                    ><i class="far fa-clock me-2"></i>6.5 hr</span
+                  >
+                  <span class="lessons"
+                    ><i class="far fa-play-circle me-2"></i>26 Lessons</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- COURSE END -->
+
+          <div class="col-xl-3 col-lg-6 col-md-6">
+            <div class="course-grid tooltip-style bg-white hover-shadow">
+              <div class="course-header">
+                <div class="course-thumb">
+                  <img
+                    src="~/assets/images/course/img_05.jpg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                  <div class="course-price">$300</div>
+                </div>
+              </div>
+
+              <div class="course-content">
+                <div class="rating mb-10">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+
+                  <span>3.9 (30 reviews)</span>
+                </div>
+                <h3 class="course-title mb-20">
+                  <a href="#">Learn How to Start an Amazon FBA Store</a>
+                </h3>
+                <div
+                  class="course-footer mt-20 d-flex align-items-center justify-content-between"
+                >
+                  <span class="duration"
+                    ><i class="far fa-clock me-2"></i>6.5 hr</span
+                  >
+                  <span class="lessons"
+                    ><i class="far fa-play-circle me-2"></i>26 Lessons</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- COURSE END -->
+
+          <div class="col-xl-3 col-lg-6 col-md-6">
+            <div class="course-grid tooltip-style bg-white hover-shadow">
+              <div class="course-header">
+                <div class="course-thumb">
+                  <img
+                    src="~/assets/images/course/img_02.jpg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                  <div class="course-price">$300</div>
+                </div>
+              </div>
+
+              <div class="course-content">
+                <div class="rating mb-10">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+
+                  <span>3.9 (30 reviews)</span>
+                </div>
+
+                <h3 class="course-title mb-20">
+                  <a href="#">Emotional Intelligence at Work: Learn Emotions</a>
+                </h3>
+
+                <div
+                  class="course-footer mt-20 d-flex align-items-center justify-content-between"
+                >
+                  <span class="duration"
+                    ><i class="far fa-clock me-2"></i>6.5 hr</span
+                  >
+                  <span class="lessons"
+                    ><i class="far fa-play-circle me-2"></i>26 Lessons</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- COURSE END -->
+
+          <div class="col-xl-3 col-lg-6 col-md-6">
+            <div class="course-grid tooltip-style bg-white hover-shadow">
+              <div class="course-header">
+                <div class="course-thumb">
+                  <img
+                    src="~/assets/images/course/img_03.jpg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                  <div class="course-price">$300</div>
+                </div>
+              </div>
+
+              <div class="course-content">
+                <div class="rating mb-10">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+
+                  <span>3.9 (30 reviews)</span>
+                </div>
+
+                <h3 class="course-title mb-20">
+                  <a href="#">Competitive Strategy law & Organization </a>
+                </h3>
+
+                <div
+                  class="course-footer mt-20 d-flex align-items-center justify-content-between"
+                >
+                  <span class="duration"
+                    ><i class="far fa-clock me-2"></i>6.5 hr</span
+                  >
+                  <span class="lessons"
+                    ><i class="far fa-play-circle me-2"></i>26 Lessons</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- COURSE END -->
+        </div>
+      </div>
+    </section>
+    <section class="features-3 section-padding-top">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-8">
+            <div class="section-heading mb-50 text-center">
+              <h2 class="font-lg">Transform Your Life</h2>
+              <p>Discover Your Perfect Program In Our Courses.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-xl-3 col-sm-6">
+            <div
+              class="feature-item feature-style-top hover-shadow rounded border-0"
+            >
+              <div class="feature-icon">
+                <i class="flaticon-teacher"></i>
+              </div>
+              <div class="feature-text">
+                <h4>Expert Teacher</h4>
+                <p>
+                  Develop skills for career of various majors including computer
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-xl-3 col-sm-6">
+            <div
+              class="feature-item feature-style-top hover-shadow rounded border-0"
+            >
+              <div class="feature-icon">
+                <i class="flaticon-layer"></i>
+              </div>
+              <div class="feature-text">
+                <h4>Self Development</h4>
+                <p>
+                  Develop skills for career of various majors including
+                  computer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-xl-3 col-sm-6">
+            <div
+              class="feature-item feature-style-top hover-shadow rounded border-0"
+            >
+              <div class="feature-icon">
+                <i class="flaticon-video-camera"></i>
+              </div>
+              <div class="feature-text">
+                <h4>Remote Learning</h4>
+                <p>
+                  Develop skills for career of various majors including language
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-xl-3 col-sm-6">
+            <div
+              class="feature-item feature-style-top hover-shadow rounded border-0"
+            >
+              <div class="feature-icon">
+                <i class="flaticon-lifesaver"></i>
+              </div>
+              <div class="feature-text">
+                <h4>Life Time Support</h4>
+                <p>
+                  Develop skills for career of various majors including language
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="testimonial-4 section-padding bg-gray">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-6">
+            <div class="section-heading text-center mb-50">
+              <h2 class="font-lg">Our Students Says</h2>
+              <p>Discover Your Perfect Program In Our Courses.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-lg-12 col-xl-12">
+            <div class="testimonials-slides owl-carousel owl-theme">
+              <div class="testimonial-item">
+                <div class="testimonial-inner">
+                  <div class="quote-icon">
+                    <i class="flaticon-left-quote"></i>
+                  </div>
+
+                  <div class="testimonial-text mb-30">
+                    Cras vel purus fringilla, lobortis libero ut Proin a velit
+                    convallis, fermentum orci in, rutrum diam. Duis elementum
+                    odio a pharetra commodo.
+                  </div>
+
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-img">
+                      <img
+                        src="~/assets/images/clients/testimonial-avata-01.jpg"
+                        alt=""
+                        class="img-fluid"
+                      />
+                    </div>
+                    <div class="testimonial-author">
+                      <h4>Cory Zamora</h4>
+                      <span class="meta">Marketing Specialist</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="testimonial-item">
+                <div class="testimonial-inner">
+                  <div class="quote-icon">
+                    <i class="flaticon-left-quote"></i>
+                  </div>
+
+                  <div class="testimonial-text mb-30">
+                    Cras vel purus fringilla, lobortis libero ut Proin a velit
+                    convallis, fermentum orci in, rutrum diam. Duis elementum
+                    odio a pharetra commodo.
+                  </div>
+
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-img">
+                      <img
+                        src="~/assets/images/clients/testimonial-avata-02.jpg"
+                        alt=""
+                        class="img-fluid"
+                      />
+                    </div>
+                    <div class="testimonial-author">
+                      <h4>Jackie Sanders</h4>
+                      <span class="meta">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="testimonial-item">
+                <div class="testimonial-inner">
+                  <div class="quote-icon">
+                    <i class="flaticon-left-quote"></i>
+                  </div>
+
+                  <div class="testimonial-text mb-30">
+                    Cras vel purus fringilla, lobortis libero ut Proin a velit
+                    convallis, fermentum orci in, rutrum diam. Duis elementum
+                    odio a pharetra commodo.
+                  </div>
+
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-img">
+                      <img
+                        src="~/assets/images/clients/testimonial-avata-03.jpg"
+                        alt=""
+                        class="img-fluid"
+                      />
+                    </div>
+                    <div class="testimonial-author">
+                      <h4>Nikolas Brooten</h4>
+                      <span class="meta">Sales Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="testimonial-item">
+                <div class="testimonial-inner">
+                  <div class="quote-icon">
+                    <i class="flaticon-left-quote"></i>
+                  </div>
+
+                  <div class="testimonial-text mb-30">
+                    Cras vel purus fringilla, lobortis libero ut Proin a velit
+                    convallis, fermentum orci in, rutrum diam. Duis elementum
+                    odio a pharetra commodo.
+                  </div>
+
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-img">
+                      <img
+                        src="~/assets/images/clients/testimonial-avata-04.jpg"
+                        alt=""
+                        class="img-fluid"
+                      />
+                    </div>
+                    <div class="testimonial-author">
+                      <h4>Terry Ambady</h4>
+                      <span class="meta">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="testimonial-item">
+                <div class="testimonial-inner">
+                  <div class="quote-icon">
+                    <i class="flaticon-left-quote"></i>
+                  </div>
+
+                  <div class="testimonial-text mb-30">
+                    Cras vel purus fringilla, lobortis libero ut Proin a velit
+                    convallis, fermentum orci in, rutrum diam. Duis elementum
+                    odio a pharetra commodo.
+                  </div>
+
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-img">
+                      <img
+                        src="~/assets/images/clients/testimonial-avata-03.jpg"
+                        alt=""
+                        class="img-fluid"
+                      />
+                    </div>
+                    <div class="testimonial-author">
+                      <h4>Nikolas Brooten</h4>
+                      <span class="meta">Sales Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="cta-5 mb--120 bg-gray">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-12">
+            <div class="cta-inner4">
+              <div class="row align-items-center justify-content-center">
+                <div class="col-xl-4 col-lg-5">
+                  <div class="cta-img mb-4 mb-lg-0">
+                    <img
+                      src="~/assets/images/about/img_9.png"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                  <div class="cta-content ps-lg-4">
+                    <span class="subheading mb-10"
+                      >Not sure where to start?</span
+                    >
+                    <h2 class="mb-20">
+                      Want to know Special Offers & Updates of new courses?
+                    </h2>
+                    <a href="#" class="btn btn-main rounded"> Join NOw</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
 
+  methods: {
+    incerment() {
+      this.count++;
+    },
+    decerment() {
+      this.count--;
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
