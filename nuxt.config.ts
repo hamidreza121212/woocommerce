90e491eff129929a864ae099932c15a1daf4a781
hamidreza121212 <www.hamidreza121212@gmail.com>
2023-01-18 13:52:19 +0330

    fix font bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,5 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
     ],
-    fontMetrics: {
-        fonts: [
-            'Inter', 
-            {
-                family: 'Some Custom Font', src: '~/ass',
-            }
-        ]
-    }
-
-    // fontMetrics: {   fonts: ['Inter', { family: 'Some Custom Font', src: '/path/to/custom/font.woff2' }],  // },
 });
 
