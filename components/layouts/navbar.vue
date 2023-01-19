3808adb60a94d6e42b4c381c3b514321cc353290
hamidreza <www.hamidreza121212@gmail.com>
2023-01-19 15:44:10 +0330

    fix top-header bug

--- components/layouts/navbar.vue
+++ components/layouts/navbar.vue
@@ -1,34 +1,40 @@
 <template>
-  <div class="h-12 grid grid-cols-6 gap-4 header-topbar text-white ">
-    <div class="mt-3 col-start-2 col-span-4 flex justify-between">
-      <div class="hidden lg:block">
-        <ul class="flex">
-          <li>
-            <span>
-              <i class="fa fa-phone-alt text-blue-400 px-2"></i>
-            </span>
-            <a href="tel:+(354) 6800 37849"> +(354) 6800 37849</a>
-          </li>
+  <div class="h-20 sm:h-12 grid md:grid-cols-2 grid-cols-1 gap-4 header-topbar text-white justify-items-center">
 
-          <li class="ml-3">
-            <span>
-              <i class="fa fa-envelope text-blue-400 px-2"></i>
-            </span>
-            <a href="malito:hello@edumel.com" class="ml-2">hello@edumel.com</a>
-          </li>
+    <div class="mt-3 hidden md:block">
+      <ul class="flex">
+        <li>
+          <span>
+            <i class="fa fa-phone-alt text-blue-400 px-2"></i>
+          </span>
+          <a href="tel:+(354) 6800 37849"> +(354) 6800 37849</a>
+        </li>
+
+        <li class="ml-3">
+          <span>
+            <i class="fa fa-envelope text-blue-400 px-2"></i>
+          </span>
+          <a href="malito:hello@edumel.com" class="ml-2">hello@edumel.com</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="mt-3">
+      <div class="grid grid-cols-1 justify-items-center sm:grid-cols-2">
+
+        <div class="">
+          <ul class="flex">
+          <li class="px-1 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+          <li class="px-1 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li class="px-1 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+          <li class="px-1 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+          <li class="px-1 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-youtube"></i></a></li>
         </ul>
-      </div>
 
-      <div class="flex">
-        <ul class="flex mr-5">
-          <li class="px-2 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li class="px-2 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li class="px-2 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-          <li class="px-2 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-          <li class="px-2 text-gray-400 hover:text-white"><a href="#"><i class="fab fa-youtube"></i></a></li>
-        </ul>
+        </div>
 
-        <ul class="flex ">
+        <div class="">
+          <ul class="flex">
           <li class="px-2 text-blue-400"><a href="#"><i class="fa fa-user-alt"></i></a>
           </li>
           <li>
@@ -46,7 +52,11 @@
             </button>
           </li>
         </ul>
+
+        </div>
+
       </div>
+
     </div>
   </div>
 </template>
